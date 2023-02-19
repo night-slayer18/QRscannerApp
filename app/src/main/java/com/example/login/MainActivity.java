f98d9c8d1e297dd6e431b11eb49bbf6ada9c1e1e
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2023-02-19 16:05:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+//import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(view -> {
+            if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/login/ScanActivity.java
@@ -0,0 +1,43 @@
+package com.example.login;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class ScanActivity extends AppCompatActivity {
+    Button btn_scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        btn_scan= findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(v->{
+            scanCode();
+        });
+    }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result -> {
+        if(result.getContents()!=null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(ScanActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", (dialog, which) -> dialog.dismiss()).show();
+        }
+    });
+}
